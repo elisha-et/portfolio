1a2b99e6d8e404015d8f95df67cc038259d0cdab
Elisha Etukudoh <elisha@Elishas-MacBook-Pro.local>
2024-08-15 22:31:52 -0400

    Made changes to about section

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,26 +1,23 @@
 import "./About.css";
 
-
-
 function About() {
-
   return (
     <>
       <div className="about">
         <div className="title">About</div>
         <div className="aboutme">
-          Passionate about soccer, coding and technology, I am a dynamic
-          individual from Nigeria with a keen interest in Computer Science.
-          Driven by a desire to achieve excellence and make a meaningful impact
-          in the world using technology, I am dedicated to leveraging my skills
-          and knowledge to contribute to both personal and societal growth.
+          With a passion for soccer, coding, and technology, I am a dynamic
+          individual with a deep interest in Computer Science. Driven by a
+          desire for excellence and a commitment to making a meaningful impact
+          through technology, I am dedicated to leveraging my skills and
+          knowledge to contribute to both personal and societal growth.
           <br />
           <br />
-          With a strong commitment to continuous learning and innovation, I aspire to
-          reach greater heights in life while actively working towards uplifting
-          my community and country, Nigeria.
+          With a strong commitment to continuous learning and innovation, I
+          aspire to reach greater heights in life while actively working towards
+          uplifting my community and making a positive impact on the world.
         </div>
-      </div>          
+      </div>
     </>
   );
 }
